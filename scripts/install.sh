bb377913cd81e333753419874c090b1a744562c5
Barak <brkbsn@gmail.com>
2024-01-01 13:51:13 +0200

    Edit the .gitignore file after adding a virtual environment and running pytest for the first time.

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 function main {
     python -m virtualenv .env --prompt "[project] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
+    find .env -name site-packages -exec bash -c 'echo "../../../../../" >> {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
 }
