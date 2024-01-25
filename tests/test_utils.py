3142e7d55de9efeb2c0c49be589cf23b0bd187d8
Barak <brkbsn@gmail.com>
2024-01-25 12:03:09 +0200

    Add test_snapshot and fix some small bugs.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ COLOR_IMAGE_HEIGHT_1 = 2
 COLOR_IMAGE_1 = b"\xff\x00\x00\xff\x00\x00\xff\x00\x00\xff\x00\x00"
 DEPTH_IMAGE_WIDTH_1 = 2
 DEPTH_IMAGE_HEIGHT_1 = 2
-DEPTH_IMAGE_1 = [1.0, 1.0, 1.0, 1.0]
+DEPTH_IMAGE_1 = (1.0, 1.0, 1.0, 1.0)
 FEELINGS_1 = (1.1, 1.2, 1.3, 1.4)
 
 # Snapshot 2.
@@ -19,5 +19,5 @@ COLOR_IMAGE_HEIGHT_2 = 3
 COLOR_IMAGE_2 = b"\x00\xff\x00\x00\xff\x00\x00\xff\x00\x00\xff\x00\x00\xff\x00\x00\xff\x00"  # noqa: E501
 DEPTH_IMAGE_WIDTH_2 = 2
 DEPTH_IMAGE_HEIGHT_2 = 3
-DEPTH_IMAGE_2 = [2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+DEPTH_IMAGE_2 = (2.0, 2.0, 2.0, 2.0, 2.0, 2.0)
 FEELINGS_2 = (2.1, 2.2, 2.3, 2.4)
